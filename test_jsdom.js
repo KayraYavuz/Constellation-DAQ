618e89c1f7761db22a965357dacc15e60819ee6c
KayraYavuz <ahmetkayrayavuz@gmail.com>
2026-08-23 14:15:40 +0200

    Fix DFP panel live throughput chart and adjust OHP grid layout to 3x3 scrollable

--- test_jsdom.js
+++ test_jsdom.js
@@ -1,20 +1,14 @@
 const fs = require('fs');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
+const { JSDOM } = require('jsdom');
 const html = fs.readFileSync('bl4s_event_explorer.html', 'utf8');
-const dom = new JSDOM(html, { runScripts: "dangerously", url: "http://localhost:5050/" });
 
-// Mock echarts and ResizeObserver
-dom.window.echarts = {
-    init: function() { return { setOption: function(){}, resize: function(){} }; }
-};
-dom.window.ResizeObserver = class { observe(){} unobserve(){} disconnect(){} };
-dom.window.fetch = async () => ({ json: async () => ({}) });
+const dom = new JSDOM(html, {
+  url: "file:///Users/kayrayavuz/Desktop/DATA/Constellation-DAQ-Git/bl4s_event_explorer.html",
+  runScripts: "dangerously",
+  resources: "usable"
+});
 
-try {
-    dom.window.openPanel('dfp_panel', 'TDAQ System', 'DFP Panel & Network', 'dfp_panel', true);
-    console.log("DFP Panel opened successfully. activePanels:", Object.keys(dom.window.activePanels));
-} catch (e) {
-    console.error("Error opening DFP Panel:", e);
-}
+dom.window.addEventListener('error', (event) => {
+  console.error("JSDOM Error:", event.error);
+});
+console.log("JSDOM loaded successfully.");
